1d2768e7b113fa3c28f5154941201d3b79a5eb4d
reyynald <rapvis3221@gmail.com>
2026-01-29 08:44:55 +0800

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,17 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        // Mengizinkan Vite untuk diakses melalui jaringan (IP)
+        host: true, 
+        // Memastikan port konsisten agar mudah diakses
+        port: 5173,
+        strictPort: true,
+        // Penting untuk Laravel: mendefinisikan URL yang akan digunakan oleh @vite di blade
+        hmr: {
+            host: '192.168.1.8', // <--- GANTI dengan IP laptop Anda
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
